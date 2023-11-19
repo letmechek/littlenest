a89044822868ce8a14addce5994a5f0ae8d7fb29
letmechek <abdullahi.ali1017@gmail.com>
2023-11-19 17:01:23 -0500

    adds search ui

--- src/components/ProductDetail/ProductDetail.jsx
+++ src/components/ProductDetail/ProductDetail.jsx
@@ -19,24 +19,24 @@ export default function ProductDetail({ user }) {
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
   let navigate = useNavigate();
   const location = useLocation();
-  const responsive = {
-    superLargeDesktop: {
-      breakpoint: { max: 4000, min: 3000 },
-      items: 1,
-    },
-    desktop: {
-      breakpoint: { max: 3000, min: 1024 },
-      items: 1,
-    },
-    tablet: {
-      breakpoint: { max: 1024, min: 464 },
-      items: 1,
-    },
-    mobile: {
-      breakpoint: { max: 464, min: 0 },
-      items: 1,
-    },
-  };
+  // const responsive = {
+  //   superLargeDesktop: {
+  //     breakpoint: { max: 4000, min: 3000 },
+  //     items: 1,
+  //   },
+  //   desktop: {
+  //     breakpoint: { max: 3000, min: 1024 },
+  //     items: 1,
+  //   },
+  //   tablet: {
+  //     breakpoint: { max: 1024, min: 464 },
+  //     items: 1,
+  //   },
+  //   mobile: {
+  //     breakpoint: { max: 464, min: 0 },
+  //     items: 1,
+  //   },
+  // };
 
   useEffect(() => {
     async function fetchVehicle() {
--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,57 +1,89 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { searchProducts } from '../../utilities/baby-products-api'
+import { Link } from 'react-router-dom';
+import BabyProductItems from '../BabyProductItems/BabyProductItems';
+import Loader from '../Loader/Loader';
+import{ MagnifyingGlassIcon} from '@heroicons/react/24/outline'
 
-export default function Search()  {
-  const [query, setQuery] = useState('');
-  const [results, setResults] = useState([]);
-  const [loading, setLoading] = useState(false);
+export default function Search() {
+    const [query, setQuery] = useState('');
+    const [results, setResults] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [hasInteracted, setHasInteracted] = useState(false);
+    const searchRef = useRef(null);
 
-  useEffect(() => {
-    const fetchSearchResults = async () => {
-      try {
-        setLoading(true);
-        const response = await searchProducts(query);
-        setResults(response); // Assuming your API response has a 'data' property
-      } catch (error) {
-        console.error('Error searching products:', error);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    // Only trigger the API call if there's a query (non-empty string)
-    if (query.trim() !== '') {
-      fetchSearchResults();
-    } else {
-      // Clear the results if the query is empty
-      setResults([]);
-    }
-  }, [query]);
-
-  return (
-    <div>
-      <input
-        type="text"
-        placeholder="Search for products..."
-        value={query}
-        onChange={(e) => setQuery(e.target.value)}
-      />
-
-      {loading && <p>Loading...</p>}
-
-      {results.length > 0 && (
-        <div>
-          <h2>Search Results:</h2>
-          <ul>
-            {results.map((product) => (
-              <li key={product.id}>{product.name}</li>
-            ))}
-          </ul>
+  
+    const closeSearch = () => {
+        setResults([]);
+        setQuery('');
+        setHasInteracted(false);
+      };
+    
+      useEffect(() => {
+        const handleDocumentClick = (event) => {
+          if (searchRef.current && !searchRef.current.contains(event.target)) {
+            setHasInteracted(false);
+          }
+        };
+    
+        document.addEventListener('click', handleDocumentClick);
+    
+        return () => {
+          document.removeEventListener('click', handleDocumentClick);
+        };
+      }, []);
+    
+      useEffect(() => {
+        const fetchSearchResults = async () => {
+          try {
+            setLoading(true);
+            const response = await searchProducts(query);
+            setResults(response.slice(0, 3));
+            setHasInteracted(true);
+          } catch (error) {
+            console.error('Error searching products:', error);
+          } finally {
+            setLoading(false);
+          }
+        };
+    
+        if (query.trim() !== '') {
+          fetchSearchResults();
+        } else {
+          setResults([]);
+        }
+      }, [query]);
+    
+      return (
+        <div className="relative" ref={searchRef}>
+         <div className="relative flex items-center">
+        <div className="absolute left-3 ">
+          <MagnifyingGlassIcon className="w-6 h-6 text-gray-500" />
         </div>
-      )}
-
-      {results.length === 0 && !loading && <p>No results found.</p>}
-    </div>
-  );
-};
-
+        <input
+          type="text"
+          placeholder="Search Our Store"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+          className="w-full pl-10 pr-4 py-2 border border-gray-300 rounded focus:outline-none focus:border-blue-500 pt-4 pb-4"
+        />
+      </div>
+    
+          {loading && <p className="mt-2 text-gray-600"><Loader/></p>}
+    
+          {hasInteracted && results.length === 0 && !loading && <p className="mt-2 text-gray-600">No results found.</p>}
+    
+          {results.length > 0 && (
+            <div className="left-0 mt-2 w-full bg-white border border-gray-300 rounded shadow-lg flex">
+              {results.map((product) => (
+                <div key={product._id} className="flex-1 max-w-1/3 aspect-w-1 aspect-h-1">
+                  <Link to={`/${product.name}/${product._id}`} className="block p-4" onClick={closeSearch}>
+                    <BabyProductItems babyProductItem={product} />
+                  </Link>
+                </div>
+              ))}
+            </div>
+          )}
+        </div>
+      );
+    }
